b870dc42773f406adff17d1f9a7fd6943f7a2838
Noppakun Anantakitthawon <pro10150@gmail.com>
2021-12-31 03:01:38 +0700

    add first menu detail

--- lib/screen/home/recipe_content.dart
+++ lib/screen/home/recipe_content.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'actions_toolbar.dart';
 import 'recipe_description.dart';
+import '../menu_detail/menu_detail.dart';
 
 class recipeContent extends StatelessWidget {
   @override
@@ -8,20 +10,28 @@ class recipeContent extends StatelessWidget {
     return Expanded(
       child: Stack(
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(left: 10),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image(
-                image: AssetImage('lib/assets/home/food.png'),
-                fit: BoxFit.cover,
-                height: 430,
-                width: 350,
-              ),
-              // decoration:
-              //     BoxDecoration(borderRadius: BorderRadius.circular(15)),
-            ),
-          ),
+          GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    PageTransition(
+                        child: MenuDetail(),
+                        type: PageTransitionType.leftToRight));
+              },
+              child: Container(
+                margin: EdgeInsets.only(left: 10),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image(
+                    image: AssetImage('lib/assets/home/food.png'),
+                    fit: BoxFit.cover,
+                    height: 430,
+                    width: 350,
+                  ),
+                  // decoration:
+                  //     BoxDecoration(borderRadius: BorderRadius.circular(15)),
+                ),
+              )),
           Row(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.end,
--- lib/screen/home/recipe_description.dart
+++ lib/screen/home/recipe_description.dart
@@ -1,28 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:page_transition/page_transition.dart';
+import '../menu_detail/menu_detail.dart';
 
 class recipeDescription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
+        child: InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            PageTransition(
+                child: MenuDetail(), type: PageTransitionType.rightToLeft));
+      },
       child: Column(
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            // Container(
-            //   margin: EdgeInsets.only(left: 10),
-            //   child: ClipRRect(
-            //     borderRadius: BorderRadius.circular(8.0),
-            //     child: Image(
-            //       image: AssetImage('assets/home/food.png'),
-            //       fit: BoxFit.cover,
-            //       height: 450,
-            //       width: 500,
-            //     ),
-            //     // decoration:
-            //     //     BoxDecoration(borderRadius: BorderRadius.circular(15)),
-            //   ),
-            // ),
             Container(
               margin: EdgeInsets.only(left: 10, top: 450),
               child: (Text(
@@ -60,6 +55,6 @@ class recipeDescription extends StatelessWidget {
               ),
             ),
           ]),
-    );
+    ));
   }
 }
