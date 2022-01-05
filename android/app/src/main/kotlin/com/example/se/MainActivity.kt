3f793206d52ce509333f76eb0f37aaf21d708773
Supitcha Jampathong <Supitcha.gmail@mail.kmutt.ac.th>
2022-01-05 15:23:01 +0700

    login,register,search finish

--- android/app/src/main/kotlin/com/example/se/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.se
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/screen/home/trending.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'recipe_content.dart';
-
-class trending extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return recipeContent();
-  }
-}
