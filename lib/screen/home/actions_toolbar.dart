dac997fd2a4948d20fc689818675148e8c17dc6d
Sahasawad Pracong <74421891+armmysaha@users.noreply.github.com>
2022-02-24 16:30:41 +0700

    Add files via upload

--- lib/screen/home/actions_toolbar.dart
+++ lib/screen/home/actions_toolbar.dart
@@ -6,6 +6,8 @@ import 'package:hewa/screen/profile/otherPeople.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:share/share.dart';
 import 'package:hewa/screen/comment.dart';
+import 'package:like_button/like_button.dart';
+
 
 class actionsToolbar extends StatelessWidget {
   Widget _getSocialAction({required String title, required IconData icon}) {
@@ -84,25 +86,57 @@ class actionsToolbar extends StatelessWidget {
         ));
   }
 
-  @override
   Widget build(BuildContext context) {
     return Container(
       width: 100,
       child: Column(
         mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end, 
         children: [
-          _getFollowAction(
-              pictureUrl:
-                  "https://secure.gravatar.com/avatar/ef4a9338dca42372f15427cdb4595ef7",
-              context: context),
-          RawMaterialButton(
-            onPressed: () {},
-            child: _getSocialAction(
-              title: '15.2k',
-              icon: MdiIcons.heartOutline,
-            ),
+          Column(
+            children: [
+              _getFollowAction(
+                  pictureUrl:
+                      "https://secure.gravatar.com/avatar/ef4a9338dca42372f15427cdb4595ef7",
+                  context: context),
+              LikeButton(
+              size: 50,
+              circleColor:
+                  CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
+              bubblesColor: BubblesColor(
+                dotPrimaryColor: Color(0xff33b5e5),
+                dotSecondaryColor: Color(0xff0099cc),
+              ),
+              likeCount: 100,
+              // countBuilder: (int count, bool isLiked, String text) {
+              //   var color = isLiked ? Colors.deepPurpleAccent : Colors.grey;
+              //   Widget result;
+              //   if (count == 0) {
+              //     result = Text(
+              //       "love",
+              //       style: TextStyle(color: color),
+              //     );
+              //   } else
+              //     result = Text(
+              //       text,
+              //       style: TextStyle(color: color),
+              //     );
+              //   return result;
+              // },
           ),
+          // RawMaterialButton(
+          //   onPressed: () {},
+          //   child: _getSocialAction(
+          //     title: '15.2k' ,
+          //     icon: MdiIcons.heartOutline,
+          //   ),
+          //   // onPressed: () async{
+          //   //   if(menu.like.contains(user.uid))
+          //   //     menu.like.remove(user.uid);
+          //   //   else menu.like.add(user.uid);
+          //   //   await DBServices().updatevehicule(menu);
+          //   // }, 
+          // ),
           RawMaterialButton(
             onPressed: () {
               navigateTocommentPage(context);
@@ -114,11 +148,27 @@ class actionsToolbar extends StatelessWidget {
               onPressed: _shareContent,
               child: _getSocialAction(
                   title: 'Share', icon: MdiIcons.shareOutline)),
+            ],
+          ),
         ],
       ),
     );
   }
 }
+
+
+// LikeButton(
+//       onTap: onLikeButtonTapped,
+//     ),
+Future<bool> onLikeButtonTapped(bool isLiked) async{
+    /// send your request here
+    // final bool success= await sendRequest();
+
+    /// if failed, you can do nothing
+    // return success? !isLiked:isLiked;
+
+    return !isLiked;
+  }
 
 navigateToProfilePage(BuildContext context) {}
 
