9fc433daea463a3e5e20171bd39c005c6998339c
Noppakun Anantakitthawon <67267679+pro10150@users.noreply.github.com>
2022-04-30 17:35:41 +0700

    release apk

---
+++ build/search_choices/generated/source/buildConfig/release/li/jod/search_choices/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package li.jod.search_choices;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "li.jod.search_choices";
+  public static final String BUILD_TYPE = "release";
+}
